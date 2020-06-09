dab4d6d67e991bb62435cc670c0cc1d1e4b440de
zhangyan <ryanstart@163.com>
2020-06-09 09:25:00 +0800

    创建项目

---
+++ crms/src/main/java/com/ryan/crms/educationController.java
@@ -0,0 +1,12 @@
+package com.ryan.crms;
+
+/**
+ * @author Ryanstart
+ * @creat 2020-06-09-9:23
+ */
+public class educationController {
+    public static void main(String[] args) {
+        System.out.println("jiajiajia");
+        System.out.println(12);
+    }
+}
